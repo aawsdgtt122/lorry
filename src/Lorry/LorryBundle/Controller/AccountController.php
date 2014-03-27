cb10acdf62737a9b7882ca47077b029267c58cf9
Benedict Etzel <developer@beheh.de>
2014-03-27 03:11:55 +0100

    add Account controller

---
+++ src/Lorry/LorryBundle/Controller/AccountController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lorry\LorryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AccountController extends Controller
+{
+    public function loginAction()
+    {
+    }
+
+    public function registerAction()
+    {
+    }
+
+    public function logoutAction()
+    {
+    }
+
+    public function settingsAction()
+    {
+    }
+
+}
