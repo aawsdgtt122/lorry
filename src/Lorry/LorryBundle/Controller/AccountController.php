8a829d0eb7d8581a8ea945d1b0e8471d5a0dda5f
Benedict Etzel <developer@beheh.de>
2014-03-27 03:29:46 +0100

    update account views

--- src/Lorry/LorryBundle/Controller/AccountController.php
+++ src/Lorry/LorryBundle/Controller/AccountController.php
@@ -4,22 +4,21 @@ namespace Lorry\LorryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AccountController extends Controller
-{
-    public function loginAction()
-    {
-    }
+class AccountController extends Controller {
 
-    public function registerAction()
-    {
-    }
+	public function loginAction() {
+		return $this->render('LorryBundle:Account:login.html.twig');
+	}
 
-    public function logoutAction()
-    {
-    }
+	public function registerAction() {
+		return $this->render('LorryBundle:Account:register.html.twig');
+	}
 
-    public function settingsAction()
-    {
-    }
+	public function logoutAction() {
+	}
+
+	public function settingsAction() {
+		return $this->render('LorryBundle:Account:settings.html.twig');
+	}
 
 }
